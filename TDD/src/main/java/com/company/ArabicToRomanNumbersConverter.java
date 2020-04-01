aa397db5b13b549f27ee702b5baeae6aba484f32
anton <atsarenko23@gmail.com>
2020-04-01 13:42:25 +0300

    Translate 2 to "II"

--- TDD/src/main/java/com/company/ArabicToRomanNumbersConverter.java
+++ TDD/src/main/java/com/company/ArabicToRomanNumbersConverter.java
@@ -3,10 +3,18 @@ package com.company;
 public class ArabicToRomanNumbersConverter {
 
     public String convert(int arabicNumber) {
-        if (arabicNumber == 0) {
-            return "";
-        } else {
+        boolean noNumber = arabicNumber == 0;
+        if (noNumber) {
+            return getDefaultMethod();
+        }
+        if (arabicNumber == 1) {
             return "I";
         }
+
+        return "II";
+    }
+
+    private String getDefaultMethod() {
+        return "";
     }
 }
--- TDD/src/test/java/com/company/WhenConvertArabicNumberToRoman.java
+++ TDD/src/test/java/com/company/WhenConvertArabicNumberToRoman.java
@@ -8,19 +8,25 @@ public class WhenConvertArabicNumberToRoman {
 
     @Test
     public void Covert_0_ToEmptyString() {
-        ArabicToRomanNumbersConverter converter = new ArabicToRomanNumbersConverter();
-
-        String romanNumber = converter.convert(0);
-
+        String romanNumber = convertToRoman(0);
         assertEquals("", romanNumber);
+    }
+
+    private String convertToRoman(int i) {
+        ArabicToRomanNumbersConverter converter = new ArabicToRomanNumbersConverter();
+        return converter.convert(i);
     }
 
     @Test
     public void Covert_1_To_I() {
-        ArabicToRomanNumbersConverter converter = new ArabicToRomanNumbersConverter();
-
-        String romanNumber = converter.convert(1);
-
+        String romanNumber = convertToRoman(1);
         assertEquals("I", romanNumber);
     }
+
+    @Test
+    public void Covert_2_To_II() {
+        String romanNumber = convertToRoman(2);
+        assertEquals("II", romanNumber);
+    }
+
 }
